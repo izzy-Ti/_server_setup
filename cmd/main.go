81361ba2cc0c6e4ec2ac266bc00b15158738006c
israel <israelashenafi29@gmail.com>
2026-01-29 13:54:55 -0800

    mysql

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/izzy-Ti/_server_setup/cmd/api"
+	"github.com/izzy-Ti/_server_setup/config"
+	"github.com/izzy-Ti/_server_setup/db"
 )
 
 func main() {
+	db, err := db.MySqlStorage(mysql.Config{
+		User:                 config.ENV.DBuser,
+		Passwd:               config.ENV.DBPassword,
+		DBName:               config.ENV.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	initStorage(db)
 	server := api.NewApiServer(":8080", nil)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("DB connected")
 }
