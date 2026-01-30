3301b59625475d35d7bfd6d846da6c26b0662b99
israel <israelashenafi29@gmail.com>
2026-01-30 14:44:41 -0800

    jwt and bcrypt

--- auth/internals/auth/handler.go
+++ auth/internals/auth/handler.go
@@ -1,24 +1,59 @@
 package auth
 
 import (
-	"encoding/json"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/izzy-Ti/_server_setup/auth/internals/db"
+	"github.com/izzy-Ti/_server_setup/auth/internals/models"
+	"github.com/izzy-Ti/_server_setup/auth/internals/utils"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type RegisterRequest struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Name     string `json: "Name"`
+	Email    string `json: "Email"`
+	Password string `json: "Password"`
 }
+type RegisterResponse struct {
+	Token string `json:"Token"`
+}
+
+var jwtSecret = []byte(os.Getenv("JWT_KEY"))
 
 func Register(w http.ResponseWriter, r *http.Request) {
 	var req RegisterRequest
-	err := json.NewDecoder(r.Body).Decode(&req)
+	err := utils.ParseJSON(r, &req)
 	if err != nil {
-		http.Error(w, "invalid request", http.StatusBadRequest)
+		utils.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	user := models.User{
+		Name:     req.Name,
+		Email:    req.Email,
+		Password: string(hashedPassword),
+	}
+	if err := db.DB.Create(&user).Error; err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": user.ID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	})
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	resp := RegisterResponse{Token: tokenString}
+	utils.WriteJson(w, http.StatusOK, resp)
 
-	// Just a test response for now
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("User registered: " + req.Username))
 }
--- auth/internals/utils/utils.go
+++ auth/internals/utils/utils.go
@@ -12,11 +12,11 @@ func ParseJSON(r *http.Request, payload any) error {
 	}
 	return json.NewDecoder(r.Body).Decode(payload)
 }
-func writeJson(w http.ResponseWriter, status int, v any) error {
+func WriteJson(w http.ResponseWriter, status int, v any) error {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(v)
 }
 func WriteError(w http.ResponseWriter, status int, err error) {
-	writeJson(w, status, map[string]string{"error": err.Error()})
+	WriteJson(w, status, map[string]string{"error": err.Error()})
 }
