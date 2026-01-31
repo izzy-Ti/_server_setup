be9e3cc44d8d80adc6694f5b7f3a854138e764ab
israel <israelashenafi29@gmail.com>
2026-01-31 12:24:59 -0800

    login handler

--- auth/internals/auth/route.go
+++ auth/internals/auth/route.go
@@ -5,4 +5,5 @@ import "github.com/gorilla/mux"
 func AuthRoutes(r *mux.Router) {
 	userRouter := r.PathPrefix("/user").Subrouter()
 	userRouter.HandleFunc("/register", Register).Methods("POST")
+	userRouter.HandleFunc("/login", Login).Methods("POST")
 }
